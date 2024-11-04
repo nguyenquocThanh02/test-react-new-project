65f5681f213b3a119778a9c0a47890d46355926a
tmtoan <minhtoantran83@gmail.com>
2024-11-04 17:03:36 +0700

    feat: detail

--- src/App.tsx
+++ src/App.tsx
@@ -21,8 +21,10 @@ function App() {
           loading="eager"
         />
         <h2>Đăng ký</h2>
+        <h3>Toàn thêm 222</h3>
         <h3>Thánh thêm vô</h3>
         <h2>Profile</h2>
+        <h2>Toàn thêm 2</h2>
       </div>
     </>
   );
